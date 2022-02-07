a32b5698ad601ad805c60abde3213ae082b1d673
rosariofirmino <73906740+rosariofirmino@users.noreply.github.com>
2022-02-07 18:33:50 -0500

    Added button + navbar components

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,29 @@
 import React, {useState} from "react";
 import { Link } from "react-router-dom";
+import { Button } from "./button";
+import './navbar.css';
+
 
 function Navbar(){
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () =>{
+        if(window.innerWidth <= 960){
+            setButton(false);
+        }else{
+            setButton(true);
+        }
+    };
+
+    window.addEventListener('resize', showButton);
     return (
         <>
-            <nav className = "navBar">
-                <div className="navBar-container">
+            <nav className = "navbar">
+                <div className="navbar-container">
                     <Link to="/" className = "navbar-logo">
                         CEO Inc
                     </Link>
@@ -47,6 +62,7 @@ function Navbar(){
                             </Link>
                         </li>
                     </ul>
+                    {button && <Button buttonStyle='btn--outline'>SEARCH JOBS</Button>}
                 </div>
             </nav>
         </>
